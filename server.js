f0cbfee8d7fa77c3744c0982118907af0843f4c0
Shawn Fox <foxeasttrojan06@gmail.com>
2021-03-12 11:15:25 -0500

    fixed waitlist

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//WE PROBABLY NEED RESERVE ARRAY TOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const tables = [];
 const waitlist = [];
 
@@ -25,34 +24,11 @@ app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.htm
 
 // Displays all tables
 app.get('/api/tables', (req, res) => res.json(tables));
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
 
-// Displays a single character, or returns false
-app.get('/api/tables/:reservation', (req, res) => {
-  const chosen = req.params.waitlist;
-
-  console.log(chosen);
-
-  /* Check each character routeName and see if the same as "chosen"
-   If the statement is true, send the character back as JSON,
-   otherwise tell the user no character was found */
-
-  for (let i = 0; i < tables.length; i++) {
-    if (chosen === tables[i].routeName) {
-      return res.json(tables[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New tables - takes in JSON input
 app.post('/api/tables', (req, res) => {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
   const newTable = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newTable
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newTable.routeName = newTable.name.replace(/\s+/g, '').toLowerCase();
   console.log(newTable);
 
@@ -60,18 +36,17 @@ app.post('/api/tables', (req, res) => {
   res.json(newTable);
 });
 
-// Starts the server to begin listening
+app.post('/api/waitlist', (req, res) => {
+  const newerTable = req.body;
 
+  newerTable.routeName = newerTable.name.replace(/\s+/g, '').toLowerCase();
+  console.log(newerTable);
 
-// Routes
+  waitlist.push(newerTable);
+  res.json(newerTable);
+});
 
-// Basic route that sends the user first to the AJAX Page+git pull
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
-app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
-app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
-
-// Displays all reservations
-app.get('/api/reservations', (req, res) => res.json(reservations));
+app.get('/api/waitlist', (req, res) => res.json(reservations));
 
 // Displays a single character, or returns false
 app.get('/api/reservations/:reservation', (req, res) => {
@@ -79,9 +54,6 @@ app.get('/api/reservations/:reservation', (req, res) => {
 
   console.log(chosen);
 
-  /* Check each character routeName and see if the same as "chosen"
-   If the statement is true, send the character back as JSON,
-   otherwise tell the user no character was found */
 
   for (let i = 0; i < reservations.length; i++) {
     if (chosen === reservations[i].routeName) {
